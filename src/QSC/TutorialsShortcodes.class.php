c15cf53881e4970941054747751e28848e98e5bb
Jim <jiminac@aol.com>
2017-10-30 19:36:54 -0700

    asdfdsss

---
+++ src/QSC/TutorialsShortcodes.class.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace QSC;
+
+class Shortcode_ContactForm{
+	
+	public function doRegisterShortcode(){
+		add_shortcode('AWS-tut-menu', array($this,'doRenderAndReturnShortcodeAWS'));
+		add_shortcode('WPcodeception-tut-menu', array($this,'doRenderAndReturnShortcodeWPcodeception'));
+		
+	}
+	
+	public function doRenderAndReturnShortcodeAWS(){
+		return "Hello World!";
+	}
+	
+	public function doRenderAndReturnShortcodeWPcodeception(){
+		return '<h2><ul>
+	<li><a href="/?p=15038">WordPress Codeception Tutorial</a></li>
+	<li><a href="/?p=14768">Your first unit test</a></li>
+	<li><a href="/?p=15106">Your first acceptance test</a></li>
+</ul></h2>';
+	}
+	
+}
