b733129a4b3e102ccabdd5e0303aaaf72d1cefdb
Jim <jiminac@aol.com>
2017-11-03 14:10:49 -0700

    4th item

--- src/QSC/TutorialsShortcodes.class.php
+++ src/QSC/TutorialsShortcodes.class.php
@@ -16,7 +16,8 @@ class TutorialsShortcodes{
 		<li><a href = "/?p=13390">Back to the Beginning</a></li>
 		<li><a href="/aws-instance/">Setup an Ubuntu instance, i.e. on AWS</a>. You should be able to do this on any cloud based <a href="https://www.ubuntu.com/" rel="noopener" target="_blank">Ubuntu</a> machine or laptop.</li>
 		<li><a href="http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-key-pairs.html#having-ec2-create-your-key-pair" target = "_blank" >Create a local key pair on your machine and connect to your instance</a>. This is the Amazon security authentication system.</li>
-		<li><a href="/?p=13410">Install and update software</a>.</li>
+		<li><a href="/?p=13410">Install and update software</a></li>
+		<li><a href = "/?p=15225">Understanding configuration files</a></li>
 </h2>';
 		
 	}
